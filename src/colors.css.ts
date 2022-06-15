0724e3f18f2ad2ca77b55d6fab218319fa5ca3b1
Pascal Collins <pascal.collins@gmx.de>
2022-06-15 22:13:04 +0200

    resets color scheme to its original

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,4 +1,4 @@
-/*export const colors = {
+export const colors = {
   background: "#ffd500",
   text: "#005bbb",
   primary: "#005bbb",
@@ -6,12 +6,4 @@
   active: "#004287",
   black: "#000",
 }
-*/
-export const colors = {
-  background: "#D6D9DD",
-  text: "#756477",
-  primary: "#EB6969",
-  muted: "#F5F0E5",
-  active: "#BE505F",
-  black: "#000",
-}
+
