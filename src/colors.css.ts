238ccc1bd603feb9b1cd1cafb700906c9915635d
Pascal Collins <pascal.collins@gmx.de>
2022-06-15 22:03:00 +0200

    changes color theme

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,8 +1,17 @@
-export const colors = {
+/*export const colors = {
   background: "#ffd500",
   text: "#005bbb",
   primary: "#005bbb",
   muted: "#f5cc00",
+  active: "#004287",
+  black: "#000",
+}
+*/
+export const colors = {
+  background: "#f6eee8",
+  text: "#756477",
+  primary: "#EDA7A7",
+  muted: "#DECFCF",
   active: "#004287",
   black: "#000",
 }
