e52e62e3ec26697c662551beb97ef8f9920d1a9b
Pascal Collins <pascal.collins@gmx.de>
2023-01-21 23:19:27 +0100

    undoes last change

--- src/components/hero.js
+++ src/components/hero.js
@@ -32,7 +32,7 @@ export default function Hero(props) {
               {props.h1}
             </Heading>
             <Subhead as="h2">{props.subhead}</Subhead>
-            <Text as="p">{props.text} TEST!</Text>
+            <Text as="p">{props.text}</Text>
             <ButtonList links={props.links} />
           </Box>
         </Flex>
